9ce838730753766c86e6cf3ab90d61adb63db24a
itut-vl <camifoure@gmail.com>
2026-03-26 00:23:42 +0900

    if文

---
+++ 基礎/if.py
@@ -0,0 +1,63 @@
+#  条件式(if文)について ====
+
+# 1. elifで条件を増やせる。
+# elseはifやelifに当てはまらなかった場合の処理。
+
+age = int(input("入力"))
+
+if age >= 20:
+    print("成人しています。")
+elif age >= 18:
+    print("成人していますが、お酒は飲んでいけません")
+else:
+    print("成人していません。")
+
+
+# 2. if文はBool値で結果を評価している。
+test = True
+
+if test:
+    print("Bool値がTrueなのでこの条件が走る。")
+
+
+
+# 3. 三項演算子
+# ifとelseによって条件を分ける時、1行で書くことができる。
+# 式：【変数 = 成功時の値 if 条件式 else 失敗時の値。】
+
+# 通常の書き方
+age = 21
+if age >= 20:
+    result = "成人です"
+else:
+    result = "成人ではありません。"
+print(result)
+
+
+# 三項演算子を使った書き方
+result = "成人です" if age >= 20 else "成人ではありません"
+print(result)
+
+
+
+# 4. Bool以外で評価されるものについて(Truthy)
+# True/False以外にも評価される対象になる値がある。
+
+# Falseとして扱われるもの
+# 0, "", [], {}, (), None
+# ｟例｠
+if 0:
+    print("実行されない")
+
+if []:
+    print("実行されない")
+
+
+# Trueとして扱われるもの
+# 1以上の数値, "hello", [1,2,3], {"key": "value"}
+# ｟例｠
+if 10:
+    print("実行される")
+if [1,2,3]:
+    print("実行される")
+
