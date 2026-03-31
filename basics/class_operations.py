5aff26394722b213839be1b52e673cfe356949f7
itut-vl <camifoure@gmail.com>
2026-03-31 20:24:55 +0900

    クラス

---
+++ basics/class_operations.py
@@ -0,0 +1,21 @@
+# クラス
+
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def print_info(self):
+        print(f"{self.name}：ユーザー名")
+        print(f"{self.age}：年齢")
+
+name = input("ユーザー名を入力してください")
+age = input("年齢を入力してください")
+
+user = Student(name,age)
+
+
+# 継承
+# クラス名の後ろに()をつけて継承したいクラスを記述する
+class NewClass(Student):
+    pass
