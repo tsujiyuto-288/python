75b6e76eb53acc6fb92b5a0483635fb9fb040531
itut-vl <camifoure@gmail.com>
2026-04-09 22:30:51 +0900

    フォーマッター

--- basics/list_operations.py
+++ basics/list_operations.py
@@ -3,7 +3,8 @@
 
 # === メソッドによる操作 ===
 
-members = ["高橋","田中","辻","佐藤"]
+members = ["高橋", "田中", "辻", "佐藤"]
+
 
 # 末尾に追加する append()
 def list_operations_1():
@@ -11,13 +12,15 @@ def list_operations_1():
     members.append(new_member)
     print(members)
 
+
 # 指定の位置に要素を追加する insert()
 # 第一引数がインデックスの位置
 def list_operations_2():
     new_member_name = input("新しいメンバーを入力してください")
     new_member_number = int(input("何番目に挿入したいですか？")) - 1
-    members.insert(new_member_number,new_member_name)
+    members.insert(new_member_number, new_member_name)
     print(members)
+
 
 # 指定したインデックスの要素を返しつつ削除　pop()
 # インデックスを指定しなかった場合末尾の要素を削除します
@@ -25,11 +28,13 @@ def list_operations_3():
     members.pop(1)
     print(members)
 
+
 # 対象の要素のインデックスを調べる　index()
 def list_operations_4():
     member = input("調べたいメンバーを入力してください")
     member_index = members.index(member)
     print(member_index)
+
 
 # 例 popとindexを組み合わせる
 def list_operations_sample():
@@ -37,6 +42,7 @@ def list_operations_sample():
     delete_index = members.index(delete_member)
     members.pop(delete_index)
     print(f"削除後のメンバーリスト{members}")
+
 
 # リストの要素を全削除　clear()
 def list_operations_5():
@@ -47,6 +53,7 @@ def list_operations_5():
 
 # === メソッド以外の操作 ===
 
+
 # 特定の要素を削除 del
 # del 要素[インデックス]　と書く
 def list_operations_6():
@@ -54,11 +61,13 @@ def list_operations_6():
     del members[1]
     print(f"削除後のリスト → {members}")
 
+
 # リストの連結 +
 def list_operations_7():
-    members_2 = ["伊藤","金子","竹内","一ノ瀬"]
+    members_2 = ["伊藤", "金子", "竹内", "一ノ瀬"]
     new_members = members + members_2
     print(new_members)
+
 
 # 要素が含まれているか確認する in
 def list_operations_8():
@@ -68,10 +77,10 @@ def list_operations_8():
     else:
         print(f"{member}はリストに含まれません。")
 
+
 # 要素の数を調べる len()
 def list_operations_9():
     print(f"要素数は{len(members)}です")
-
 
 
 # リスト内包表記
@@ -91,10 +100,6 @@ def list_operations_10():
     print(members_index2)
 
     # 補足：条件式をつけることもできる
-    members_index3 = [
-        members.index(member)
-        for member in members 
-        if member == "辻"
-    ]
+    members_index3 = [members.index(member) for member in members if member == "辻"]
     print("=== リスト内包表記｟条件式あり｠ ===")
     print(members_index3)
