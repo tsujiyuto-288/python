205149dc56da02d0e30be2378eaf84026e904860
itut-vl <camifoure@gmail.com>
2026-04-18 00:36:16 +0900

    stripについてまとめるファイル変更

--- basics/str_operations.py
+++ basics/str_operations.py
@@ -54,3 +54,19 @@ def str_operations_5():
     message_2 = "今日は{1}年{2}月{0}日です。".format(day, year, month)
     print(message_1)
     print(message_2)
+
+# strip()
+# 文字列の前後の文字を削除する
+def str_operations_6():
+    # 文字列の最初と最後の空欄を除去してくれる。
+    name = input("名前を入力してください。")
+    print(name)
+    strip_name = name.strip()
+    print(strip_name)
+
+    # 引数を指定すると削除対象が引数で指定したものになる。。
+    eng_name = input("英語で名前を入力してください。")
+    print(f"{eng_name}：削除される前")
+    delete_text = input("削除したいアルファベット(削除は最初と最後に対してだけ)")
+    format_eng_name = eng_name.strip(delete_text)
+    print(f"{format_eng_name}：指定したアルファベットが削除されたもの。")
